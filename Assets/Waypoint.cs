951334a1c0894b9158324020e6c3b9aae893335d
Dylan Nelkin <dylannelkin@gmail.com>
2020-01-30 15:47:19 -0500

    -added waypoint.cs

---
+++ Assets/Waypoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Script for waypoints on the overworld map
+public class Waypoint : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
