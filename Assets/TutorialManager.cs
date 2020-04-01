21ebb44ca145df11f4e50d0f5b36a465ff3fa26c
Dylan Nelkin <dylannelkin@gmail.com>
2020-04-01 08:44:39 -0400

    Starting tutorial manager

---
+++ Assets/TutorialManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+   public static TutorialManager Instance = null;
+
+   public int TutorialStep;
+   
+   private void Awake()
+   {
+      if (Instance == null)
+         Instance = this;
+      else if(Instance != this)
+         Destroy(gameObject);
+   }
+
+   private void Start()
+   {
+      TutorialStep = 0;
+   }
+}
