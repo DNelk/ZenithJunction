866f1c806ef2400a16c1e1e16968563449021459
Dylan Nelkin <dylannelkin@gmail.com>
2020-04-02 14:50:51 -0400

    more tutorial

--- Assets/TalkingHead.cs
+++ Assets/TalkingHead.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TalkingHead : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Image _characterSprite;
+    private TMP_Text _characterName;
+    private TMP_Text _dialogue;
+
+    public Sprite Sprite
     {
-        
+        get => _characterSprite.sprite;
+        set => _characterSprite.sprite = value;
     }
 
-    // Update is called once per frame
-    void Update()
+    public string Name
     {
-        
+        get => _characterName.text;
+        set => _characterName.text = value;
     }
+    
+    public string Dialogue
+    {
+        get => _dialogue.text;
+        set => _dialogue.text = value;
+    }
+    private void Awake()
+    {
+        _characterSprite = transform.Find("Character").GetComponent<Image>();
+        _characterName = transform.Find("Name").GetComponentInChildren<TMP_Text>();
+        _dialogue = transform.Find("DialogueBox").GetComponentInChildren<TMP_Text>();
+    }
+    
+    
 }
