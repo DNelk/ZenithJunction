88f9e2f904a60a9f0b92537b6fc4d1b5f095878c
palmmyvillage <43654403+palmmyvillage@users.noreply.github.com>
2020-06-08 17:08:48 -0400

    check in and update the status change effect

---
+++ Assets/Scripts/UI/Palmmy_UI/SetActiveFunction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveFunction : MonoBehaviour
+{
+    public void TurnGameObjectOff()
+    {
+        gameObject.SetActive(false);
+    }
+}
